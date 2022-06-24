e362f0f78a3a2f22f5205b33780fbe66dac9efb1
aozeyu <3484766443@qq.com>
2022-06-24 17:28:01 +0800

    添加message类

---
+++ src/main/java/com/yzy/test/entity/Message.java
@@ -0,0 +1,11 @@
+package com.yzy.test.entity;
+
+/**
+ * @program: test
+ * @description: 333
+ * @packagename: com.yzy.test.entity
+ * @author: 姚泽宇
+ * @date: 2022-06-24 17:27
+ **/
+public class Message {
+}
